ca6b55f4aaad26acb06bd06626aaa8e88007ba88
Amit Deshpande <amitdesh2014@gmail.com>
2020-07-01 23:50:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,20 @@
 def reverse_each_word(string)
   new_array = []
+<<<<<<< HEAD
   sentence = string.split(" ")
   sentence.collect do |words|
     new_array << words.reverse
   end
   new_array.join(" ")
+=======
+  words = string.split(" ")
+  words.each do |letter|
+    letters.each do |chars|
+      new_array = letters.unshift(chars)
+    end
+  end
+  p new_array
+>>>>>>> 791ab53e210364d77887ccdae83a694aa19a5aa0
 end
 
   
